6b496b0e33ef09ceed532b70377da02386b4e5a1
Rafał Paliwoda <r.paliwoda992@gmail.com>
2020-09-15 11:37:11 +0100

    adding Joke Service

---
+++ src/main/java/com/pali/jokeapp/controllers/JokeController.java
@@ -0,0 +1,27 @@
+package com.pali.jokeapp.controllers;
+
+
+import com.pali.jokeapp.Services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+
+        model.addAttribute("joke",jokeService.getJokes());
+
+        return "chucknorris";
+    }
+}
